54606077c51c5106dffe12797264fd0234b6c07e
Lise Poirier <lise.p.poirier@gmail.com>
2016-07-26 16:49:38 +0200

    CSS + JS

--- js/script.js
+++ js/script.js
@@ -1,5 +1,94 @@
-/* * * * * * * * * * * * * * *
- * * * * * * * * * * * * * * *
- * * YOUR CODE GOES HERE ! * *
- * * * * * * * * * * * * * * *
- * * * * * * * * * * * * * * */
+//---------- Variables ----------
+
+var temps = 1;
+var modePause = false;
+var secInMin = 60;
+var minuteurMin;
+var minuteurSec;
+var stopper;
+var replay;
+
+//---------- Fonction démarrage du minuteur ----------
+
+function lancerMinuteur() {
+    if (!minuteurSec) {
+        minuteurMin = temps;
+        minuteurSec = 0;
+        stopper = setInterval(decrementerSec, 1000);
+    } else {
+        stopper = setInterval(decrementerSec, 1000);
+    }
+
+    $("#play").prop("disabled", true);
+    $("#pause").prop("disabled", false);
+    $("#stop").prop("disabled", false);
+    
+}
+
+function interface() {
+    document.getElementById("time").innerHTML = minuteurMin + ":" + minuteurSec;
+    document.getElementById("time-title").innerHTML = minuteurMin + ":" + minuteurSec;
+    if (minuteurMin == 0 && minuteurSec == 0) {
+        document.getElementById("time").innerHTML = "5" + ":" + "00";
+        document.getElementById("time-title").innerHTML = "5" + ":" + "00";
+    }
+}
+
+function decrementerSec() {
+    if (minuteurSec == 0 && minuteurMin != 0) {
+        minuteurSec = 59;
+        minuteurMin--;
+    } else if (minuteurMin == 0 && minuteurSec == 0) {
+        if(modePause){
+            minuteurSec = 59;
+            minuteurMin= 4;
+            modePause = false;
+        }else{
+            minuteurSec = 59;
+            minuteurMin = 4;
+            modePause = true;
+        }
+        
+    } else {
+        minuteurSec--;
+    }
+
+    interface();
+}
+
+//---------- Fonction mettre sur pause ----------
+
+function stop() {
+    if (document.getElementById("play").disabled = true) {
+        replay = clearInterval(stopper);
+        $("#play").prop("disabled", false);
+        $("#pause").prop("disabled", true);
+        $("#stop").prop("disabled", false);
+    }
+}
+
+//---------- Fonction remise à zero ----------
+
+function remiseZero() {
+    if (document.getElementById("play").disabled = true) {
+        clearInterval(stopper);
+        minuteurMin = temps;
+        minuteurSec = 0;
+        document.getElementById("time").innerHTML = "25" + ":" + "00";
+        document.getElementById("time-title").innerHTML = "25" + ":" + "00";
+        $("#play").prop("disabled", false);
+        $("#pause").prop("disabled", false);
+        $("#stop").prop("disabled", true);
+    }
+}
+
+//---------- Tâche à accomplir ----------
+
+function tache() {
+    var consigne = document.getElementById("task").value;
+    var objetLi = document.createElement("li");
+    objetLi.innerHTML = consigne;
+    var objetUl = document.getElementById("done");
+    objetUl.insertBefore(objetLi, objetUl.firstChild);
+    $("li").addClass("list-group-item");
+}
